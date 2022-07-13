d1d896af2f829a53310f0522a5d9b5bb5e1780d0
myeongu <ausrndi@gmail.com>
2022-07-14 02:30:55 +0900

    Timeline 포스팅 목록 조건에 날짜 범위 지정하기

--- instagram/models.py
+++ instagram/models.py
@@ -4,18 +4,25 @@ from django.db import models
 from django.urls import reverse
 
 
-class Post(models.Model):
+class BaseModel(models.Model): # 공통된 필드가 존재할 경우 추상화
+    created_at = models.DateTimeField(auto_now_add=True) # auto_now_add
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Post(BaseModel):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     photo = models.ImageField(upload_to="instagram/post/%Y/%m/%d")
     caption = models.TextField()
     tag_set = models.ManyToManyField('Tag', blank=True)
     location = models.CharField(max_length=100)
+    
 
     def __str__(self) -> str:
         return self.caption
     
-
-
     def extract_tag_list(self):
         tag_name_list = re.findall(r"#([a-zA-Z\dㄱ-힣]+)", self.caption)
         tag_list = []
